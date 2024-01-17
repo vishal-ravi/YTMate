b8df5e985ff4ca57d0763cb565fb79744b6f0a2f
Vishal Ravi <98887033+vishal-ravi@users.noreply.github.com>
2024-01-18 01:53:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -60,5 +60,5 @@ def video_info():
     except Exception as e:
         return jsonify({'error': str(e)})
 
- # if __name__ == '__main__':
- #    app.run(debug=True)
+  if __name__ == '__main__':
+     app.run(debug=True)
